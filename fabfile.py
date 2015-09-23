fd99df2965d1062b453798ce3fd225bb38da02c3
megacreep <limuyang08@gmail.com>
2015-09-23 20:40:18 +0800

    FIX: remove logging.

--- fabfile.py
+++ fabfile.py
@@ -14,6 +14,7 @@ def deploy():
         run('mkdir -p /var/www/goddess')
 
     with cd('/var/www/goddess'):
+        run('git reset --hard')
         run('git pull')
         run('python manage.py collectstatic --noinput')
         # Migrate and Update the database
